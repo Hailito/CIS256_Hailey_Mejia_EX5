612f963cb6ddf7c80a74bc8b153b344616911551
Hailito <HAI2135351@maricopa.edu>
2025-11-25 23:55:21 -0700

    added title details and html

--- cis256_spring2022_flask 2/searchwtf.py
+++ cis256_spring2022_flask 2/searchwtf.py
@@ -17,7 +17,7 @@ class ByAuthorIdWTF(FlaskForm):
 class ByPublisherIdWTF(FlaskForm):
     mydb = BooksDB()
     publishers = mydb.getpublishers()
-    publisher_choice = SelectField("AuthorChoice", choices=publishers)
+    publisher_choice = SelectField("PublisherChoice", choices=publishers)
 
 class ByTitleWTF(FlaskForm):
     # This will also need a stringfield for the words the user types
